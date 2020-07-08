3550eb53599ce6067891ef697560a3bd53207b89
Techoplite <oriccimirko@gmail.com>
2020-07-08 09:53:28 +0100

    Add hidden style (no fuction)

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Auth = () => {
     return (
-        <div className="col-3 col-s-4 col-xs-12" id="auth">
+        <div className="col-3 col-s-4 col-xs-12 " id="auth">
             <a href="/register/" className="auth-link">Register</a>
             <a href="/signin/" className="auth-link">Sign in</a>
         </div>
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Cart = () => {
     return (
-        <div className="col-1 col-s-1 col-xs-12" id="cart">
+        <div className="col-1 col-s-1 col-xs-12 " id="cart">
             <a href="/cart/">
                 <i className="fas fa-shopping-cart"></i>
             </a>
