3627b3e29b8d8c2819dfaa0ae6d925a852368d8f
Techoplite <oriccimirko@gmail.com>
2020-07-08 09:39:35 +0100

    Fix verical navbar style

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo = () => {
     return (
         <div className="col-1 col-s-2 col-xs-8" id="logo">
-            <a href="/">
+            <a href="/" id="logo">
                 eBazar
             </a>
         </div>
