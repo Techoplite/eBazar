993ab0597947d923e83bf2a35ae9d8b5a82bf49d
Techoplite <oriccimirko@gmail.com>
2020-07-09 10:43:07 +0100

    Change searchbox style

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -3,7 +3,7 @@ import SearchBox from './SearchBox';
 
 const Aside = () => {
     return (
-        <div className="col-3 col-s3 col-xs-12" id="aside">
+        <div className="col-3 col-s-12 col-xs-12" id="aside">
             <SearchBox />
         </div>
     );
