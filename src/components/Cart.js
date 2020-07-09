83823616df279cfd8c187f640feaa33accdc9ad5
Techoplite <oriccimirko@gmail.com>
2020-07-09 13:21:09 +0100

    Add cart badge

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,6 +5,7 @@ const Cart = (props) => {
         <div className={`col-1 col-s-1 col-xs-12 ${props.toggle}`} id="cart">
             <a href="/cart/">
                 <i className="fas fa-shopping-cart"></i>
+                <span className="cart-itmes">4</span>
             </a>
         </div>
     );
