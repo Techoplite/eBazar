74f2dce6ec05a4432d426a5ea0f30af7425ab0da
Techoplite <oriccimirko@gmail.com>
2020-07-22 13:41:38 +0100

    Fix  navbar responsiveness

--- src/components/header/navbar/Cart.js
+++ src/components/header/navbar/Cart.js
@@ -3,8 +3,10 @@ import React from 'react';
 const Cart = (props) => {
     return (
         <a href="/cart/" className="cart">
-            <i className="fas fa-shopping-cart" id="cart-icon"></i>
-            <span >4</span>
+            <i className="fas fa-shopping-cart" id="cart-icon">
+                <span >4</span>
+            </i>
+            
         </a>
     );
 }
