97cae7b465b74432dbe6208494c5ac56d51abfa0
Techoplite <oriccimirko@gmail.com>
2020-07-23 12:28:31 +0100

    Fix span / Add image to item

--- src/components/content/main-content/items-list/item/Item.js
+++ src/components/content/main-content/items-list/item/Item.js
@@ -4,6 +4,10 @@ const Item = () => {
     return (
         <div className="item">
             <img src={require("./laptop.jpg")} />
+            <div className="item-details">
+                <div className="item-name">ADMI GAMING PC PACKAGE:, AMD QUAD Core A10-9700 Graphics, 1TB Hard Drive, 8GB 2666Mhz DDR4 RAM, 300Mbps Wifi, Pre-Installed with </div>
+                <div className="item-price">£375.00</div>
+            </div>
         </div>
     );
 }
