ce9632e6d22e796090d7adef5c4caf9ea0bcded4
Techoplite <oriccimirko@gmail.com>
2020-08-03 10:44:57 +0100

    Add items images/Style item description

--- src/components/content/main-content/items-list/item/Item.js
+++ src/components/content/main-content/items-list/item/Item.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react';
 const Item = (props) => {
     return (
         <Fragment>
-            <img src={require("./laptop.jpg")} alt="" />
+            <img src={process.env.PUBLIC_URL + `${props.image}`} alt="" />
             <div className="item-details">
                 <div className="item-name">{props.name}</div>
                 <div className="item-price">£{props.price}</div>
