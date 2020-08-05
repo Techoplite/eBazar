729b6fcf1d2bbd425e5cb095d8915b2ff4f88062
Techoplite <oriccimirko@gmail.com>
2020-08-05 10:25:52 +0100

    Add responsive feedback tooltip

--- src/components/content/main-content/items-list/ItemsList.js
+++ src/components/content/main-content/items-list/ItemsList.js
@@ -25,12 +25,14 @@ const ItemsList = () => {
     return (
         <div className="items-list">
             {items && items.map(item => {
+                const itemObject = item
+
                 const avarageRating = ((item.rating[1] + 2 * item.rating[2] + 3 * item.rating[3] + 4 * item.rating[4] + 5 * item.rating[5]) / (item.rating[1] + item.rating[2] + item.rating[3] + item.rating[4] + item.rating[5])).toFixed(1)
-                
+
                 const totalFeedbacks = (item.rating[1] + item.rating[2] + item.rating[3] + item.rating[4] + item.rating[5])
 
                 return <div className="item" key={item.name}>
-                    <Item name={item.name} value={item.value} price={item.price} image={item.image} discount={item.discount} rating={avarageRating} totalFeedbacks={totalFeedbacks} left={item.left}/>
+                    <Item name={item.name} value={item.value} price={item.price} image={item.image} discount={item.discount} rating={avarageRating} totalFeedbacks={totalFeedbacks} left={item.left} item={itemObject} />
                 </div>
             }
             )}
--- src/components/content/main-content/items-list/item/Item.js
+++ src/components/content/main-content/items-list/item/Item.js
@@ -1,13 +1,26 @@
 import React, { Fragment } from 'react';
 
 const Item = (props) => {
+    const star1 = props.item.rating[1]
+    const star2 = props.item.rating[2]
+    const star3 = props.item.rating[3]
+    const star4 = props.item.rating[4]
+    const star5 = props.item.rating[5]
+    const star1Percentage = star1 * 100 / props.totalFeedbacks
+    const star2Percentage = star2 * 100 / props.totalFeedbacks
+    const star3Percentage = star3 * 100 / props.totalFeedbacks
+    const star4Percentage = star4 * 100 / props.totalFeedbacks
+    const star5Percentage = star5 * 100 / props.totalFeedbacks
+
+
+
     return (
         <Fragment>
             <img src={process.env.PUBLIC_URL + `${props.image}`} alt="" />
             <div className="item-details">
                 <div className="item-name">{props.name}</div>
                 <div className="weighted-avarage-rating">{props.rating} out of {props.totalFeedbacks} feedbacks</div>
-                <div className="star-rating">
+                <div className="star-rating tooltip">
                     {props.rating > 1 ? <span className="material-icons">
                         star
                             </span> : <span className="material-icons">
@@ -33,6 +46,58 @@ const Item = (props) => {
                             </span> : <span className="material-icons">
                             star_outline
                             </span>}
+                    <span className="tooltip-content">
+                        <label htmlFor="5-star" className="progress-bar-label">5 star
+                        </label>
+                        <div className="progress" id="5-star">
+                            <div
+                                className="progress-bar"
+                                style={{ width: `${star5Percentage}%` }}>
+                            </div>
+                        </div>
+                        <div className="star-percentage">{star5Percentage.toFixed(0)}%
+                        </div>
+                        <label htmlFor="4-star" className="progress-bar-label">4 star
+                        </label>
+                        <div className="progress" id="4-star">
+                            <div
+                                className="progress-bar"
+                                style={{ width: `${star4Percentage}%` }}>
+                            </div>
+                        </div>
+                        <div className="star-percentage">{star4Percentage.toFixed(0)}%
+                        </div>
+                        <label htmlFor="3-star" className="progress-bar-label">3 star
+                        </label>
+                        <div className="progress" id="3-star">
+                            <div
+                                className="progress-bar"
+                                style={{ width: `${star3Percentage}%` }}>
+                            </div>
+                        </div>
+                        <div className="star-percentage">{star3Percentage.toFixed(0)}%
+                        </div>
+                        <label htmlFor="2-star" className="progress-bar-label">2 star
+                        </label>
+                        <div className="progress" id="2-star">
+                            <div
+                                className="progress-bar"
+                                style={{ width: `${star2Percentage}%` }}>
+                            </div>
+                        </div>
+                        <div className="star-percentage">{star2Percentage.toFixed(0)}%
+                        </div>
+                        <label htmlFor="1-star" className="progress-bar-label">1 star
+                        </label>
+                        <div className="progress" id="1-star">
+                            <div
+                                className="progress-bar"
+                                style={{ width: `${star1Percentage}%` }}>
+                            </div>
+                        </div>
+                        <div className="star-percentage">{star1Percentage.toFixed(0)}%
+                        </div>
+                    </span>
                 </div>
                 {props.left < 6 && <div className="items-left">Only {props.left} items left!</div>}
                 {props.discount === 0 ? <div className="item-price">£{props.price}</div> :
