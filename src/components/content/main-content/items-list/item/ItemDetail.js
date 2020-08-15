a9b5e78e072c3b9d7c40cf875aacf4e81ee3bb14
Techoplite <oriccimirko@gmail.com>
2020-08-15 12:07:13 +0100

    Add smartphone itemDetail style

--- src/components/content/main-content/items-list/item/ItemDetail.js
+++ src/components/content/main-content/items-list/item/ItemDetail.js
@@ -20,23 +20,147 @@ const ItemDetail = (props) => {
         return () => dispatch(actions.setAside(true))
     }, [dispatch, props.match.params])
 
+    const avarageRating =
+        currentItem !== null &&
+        ((currentItem.rating[1] + 2 * currentItem.rating[2] + 3 * currentItem.rating[3] + 4 * currentItem.rating[4] + 5 * currentItem.rating[5]) / (currentItem.rating[1] + currentItem.rating[2] + currentItem.rating[3] + currentItem.rating[4] + currentItem.rating[5])).toFixed(1)
+
+
+    const totalFeedbacks =
+        currentItem !== null &&
+        (currentItem.rating[1] + currentItem.rating[2] + currentItem.rating[3] + currentItem.rating[4] + currentItem.rating[5])
+
+    const star1Percentage =
+        currentItem !== null &&
+        currentItem.rating[1] * 100 / totalFeedbacks
+    const star2Percentage =
+        currentItem !== null &&
+        currentItem.rating[2] * 100 / totalFeedbacks
+    const star3Percentage =
+        currentItem !== null &&
+        currentItem.rating[3] * 100 / totalFeedbacks
+    const star4Percentage =
+        currentItem !== null &&
+        currentItem.rating[4] * 100 / totalFeedbacks
+    const star5Percentage =
+        currentItem !== null &&
+        currentItem.rating[5] * 100 / totalFeedbacks
+
     return (
         currentItem !== null &&
-        <div id="item-detail">
-            <div id="picture">
+        <div className="item">
+            <div className="picture">
                 <img src={process.env.PUBLIC_URL + `${currentItem.image}`} alt="" />
             </div>
-            <div id="details">
-                <p>{currentItem.name}</p>
-                <p>{currentItem.features}</p>
-                <p>{currentItem.price}</p>
-                <p>rate 1{currentItem.rating[0]}</p>
-                <p>rate 2{currentItem.rating[1]}</p>
-                <p>rate 3{currentItem.rating[3]}</p>
-                <p>rate 4{currentItem.rating[4]}</p>
-                <p>rate 5{currentItem.rating[5]}</p>
+            <div className="details">
+                <br/>
+                <div className="item-name">
+                    {currentItem.name}
+                </div>
+
+                <div className="weighted-avarage-rating">{avarageRating} out of {totalFeedbacks} feedbacks</div>
             </div>
-        </div >
+            <div className="star-rating tooltip">
+                {avarageRating > 1 ? <span className="material-icons">
+                    star
+                            </span> : <span className="material-icons">
+                        star_outline
+                            </span>}
+                {avarageRating > 2 ? <span className="material-icons">
+                    star
+                            </span> : <span className="material-icons">
+                        star_outline
+                            </span>}
+                {avarageRating > 3 ? <span className="material-icons">
+                    star
+                            </span> : <span className="material-icons">
+                        star_outline
+                            </span>}
+                {avarageRating > 4 ? <span className="material-icons">
+                    star
+                            </span> : <span className="material-icons">
+                        star_outline
+                            </span>}
+                {avarageRating > 5 ? <span className="material-icons">
+                    star
+                            </span> : <span className="material-icons">
+                        star_outline
+                            </span>}
+            </div >
+            <div className="feedback">
+                <br/>
+                Feedback
+                 <br/>
+                 <br/>
+                <div className="feedback-row">
+                    <label htmlFor="5-star" className="progress-bar-label  feedback-progress-bar">5 star
+                        </label>
+                    <div className="feedback-progress" id="5-star">
+                        <div
+                            className="progress-bar"
+                            style={{ width: `${star5Percentage}%` }}>
+                        </div>
+                    </div>
+                    <div className="feedback-star-percentage">{star5Percentage.toFixed(0)}%
+                        </div>
+                </div>
+                <div className="feedback-row">
+                    <label htmlFor="4-star" className="progress-bar-label  feedback-progress-bar">4 star
+                        </label>
+                    <div className="feedback-progress" id="4-star">
+                        <div
+                            className="progress-bar"
+                            style={{ width: `${star4Percentage}%` }}>
+                        </div>
+                    </div>
+                    <div className="feedback-star-percentage">{star4Percentage.toFixed(0)}%
+                        </div>
+                </div>
+                <div className="feedback-row">
+                    <label htmlFor="3-star" className="progress-bar-label  feedback-progress-bar">3 star
+                        </label>
+                    <div className="feedback-progress" id="3-star">
+                        <div
+                            className="progress-bar"
+                            style={{ width: `${star3Percentage}%` }}>
+                        </div>
+                    </div>
+                    <div className="feedback-star-percentage">{star3Percentage.toFixed(0)}%
+                        </div>
+                </div>
+                <div className="feedback-row">
+                    <label htmlFor="2-star" className="progress-bar-label  feedback-progress-bar">2 star
+                        </label>
+                    <div className="feedback-progress" id="2-star">
+                        <div
+                            className="progress-bar"
+                            style={{ width: `${star2Percentage}%` }}>
+                        </div>
+                    </div>
+                    <div className="feedback-star-percentage">{star2Percentage.toFixed(0)}%
+                        </div>
+                </div>
+                <div className="feedback-row">
+                    <label htmlFor="1-star" className="progress-bar-label  feedback-progress-bar">1 star
+                        </label>
+                    <div className="feedback-progress" id="1-star">
+                        <div
+                            className="progress-bar"
+                            style={{ width: `${star1Percentage}%` }}>
+                        </div>
+                    </div>
+                    <div className="feedback-star-percentage">{star1Percentage.toFixed(0)}%
+                        </div>
+                </div>
+                <div>
+                    <br/>
+                    Features
+                    <ul>
+                        {currentItem.features.map(feature => <li>{feature}</li>)}
+                    </ul>
+                    <br/>
+                </div>
+            </div>
+        </div>
     );
 }
 
