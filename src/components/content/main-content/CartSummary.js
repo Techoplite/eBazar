e3bee17847552dc588142dc6f67a103a8cb7f606
Techoplite <oriccimirko@gmail.com>
2020-08-24 11:15:54 +0100

    Fix cartSummary style

--- src/components/content/main-content/CartSummary.js
+++ src/components/content/main-content/CartSummary.js
@@ -5,41 +5,47 @@ const CartSummary = () => {
     const mapState = (state) => {
         return {
             cart: state.cart,
+            aside: state.aside
         }
     }
 
-    const { cart } = useSelector(mapState)
+    const { cart, aside } = useSelector(mapState)
 
     const getSubtotal = (item) => {
-        const subtotal = ((100 - item.discount) / 100 * item.price).toFixed(2) * item.quantity 
-        return subtotal
+        const subtotal = ((100 - item.discount) / 100 * item.price).toFixed(2) * item.quantity
+        return subtotal.toFixed(2)
     }
 
     const getTotal = (cartItems) => {
         const subtotals = []
         cartItems.map(item => {
-            const subtotal = ((100 - item.discount) / 100 * item.price).toFixed(2) * item.quantity 
-            subtotals.push(subtotal)
+            const subtotal = ((100 - item.discount) / 100 * item.price) * item.quantity
+            subtotals.push(subtotal.toFixed(2))
         })
-        const total = subtotals.reduce((a, b) => a + b, 0)
-        return total.toFixed(2)
+        let total = subtotals.reduce((a, b) => parseFloat(a) + parseFloat(b), 0)
+        total = parseFloat(total).toFixed(2)
+        return total
+    }
+
+    const getAside = () => {
+        return aside ? "with-aside" : ""
     }
 
     return (
-        <div className="cart-summary">
+        <div className={`cart-summary ${getAside()}`}>
             <p className="user-cart-name">{cart.user}'s cart</p>
             {!cart.items.length && <p className="empty-cart">Your cart is empty</p>}
             <ul>
                 {cart.items.map(item => <div key={item.id} className="cart-item">
                     <li className="name-and-quantity">{item.quantity} x {item.name}
                     </li>
-                    <li className= "subtotal" > Subtotal: <br/>£{getSubtotal(item)}
+                    <li className="subtotal" > Subtotal: <br />£{getSubtotal(item)}
                     </li>
-        </div>)
-}
+                </div>)
+                }
 
             </ul >
-    <p className="total">Total: £{getTotal(cart.items)}</p>
+            <p className="total">Total: £{getTotal(cart.items)}</p>
         </div >
     );
 }
