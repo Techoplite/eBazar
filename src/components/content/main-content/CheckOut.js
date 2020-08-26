c3bde6a5a8e553c71b7b9c9baebf109e766f2a09
Techoplite <oriccimirko@gmail.com>
2020-08-26 11:05:00 +0100

    Add CheckOut list

--- src/components/content/main-content/CheckOut.js
+++ src/components/content/main-content/CheckOut.js
@@ -1,7 +1,69 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import * as actions from '../../../actions'
+
+
 
 const CheckOut = () => {
-    return ( <div>this is the checkout page</div> );
+
+    const mapState = (state) => {
+        return {
+            cart: state.cart
+        }
+    }
+
+    const dispatch = useDispatch()
+    const { cart } = useSelector(mapState)
+
+
+    return (
+        <ul className="items-list">
+            {cart.items.map(item => {
+                return (
+                    <li
+                        className="checkout-item"
+                        key={item.name}
+                    >
+                        <img src={process.env.PUBLIC_URL + `${item.image}`} alt="" />
+                        <div className="item-details">
+                            <div className="item-name">
+                                {item.name}
+                            </div>
+                            {item.left < 6 && <div className="items-left">Only {item.left} items left!</div>}
+                            {item.discount === 0 ? <div className="item-price">£{item.price}</div> :
+                                <Fragment>
+                                    <div className="item-discount">{item.discount}% Discount</div>
+                                    <div className="item-was-price">Was £{item.price}</div>
+
+                                    <div className="item-price">Now £{((100 - item.discount) / 100 * item.price).toFixed(2)}
+                                    </div>
+                                </Fragment>}
+                        </div>
+                        <div className="order">
+                            {
+                                item.quantity > 0 &&
+                                <Fragment>
+                                    <button className="checkout-decrease" onClick={() => item.quantity === 1 ? dispatch(actions.removeItem(item)) : dispatch(actions.decreaseQuantity(item))}>-
+                            </button>
+                                    <div className="checkout-quantity">
+                                        {item.quantity}
+                                    </div>
+                                </Fragment>
+                            }
+                            <button className="checkout-add-to-cart" onClick={() => dispatch(actions.increaseQuantity(item))}>+
+                    </button>
+                            {
+                                item.quantity > 0 &&
+                                <button className="checkout-remove-item" onClick={() => dispatch(actions.removeItem(item))}>Remove item
+                    </button>
+                            }
+                        </div>
+                    </li>
+                )
+            }
+            )}
+        </ul>
+    );
 }
- 
+
 export default CheckOut;
